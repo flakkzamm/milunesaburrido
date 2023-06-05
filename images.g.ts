e5acbbbe676819fc65c3a7e895e82e6bf4c5d029
flakkzamm <135653989+flakkzamm@users.noreply.github.com>
2023-06-05 12:31:47 -0600

    Archivos iniciales para el proyecto MakeCode

---
+++ images.g.ts
@@ -0,0 +1,249 @@
+// Código generado automáticamente. No editar.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "fire":return img`
+. . . . f f . . . . f f . . . . 
+. . . 3 3 2 f f f f 2 f . . . . 
+. . . f 2 2 2 2 2 2 2 2 f . . . 
+. . f 2 2 2 2 2 2 2 2 2 2 f . . 
+. . f 5 f 5 5 5 5 5 5 f 5 f . . 
+. f 5 f 1 f 5 5 5 5 f 1 f 5 f . 
+f 5 5 f 1 1 f 5 5 f 1 1 f 5 5 f 
+f 5 5 5 f f f 5 5 f f f 5 5 5 f 
+. f 5 5 5 5 5 5 5 5 5 5 5 5 f . 
+. . f 5 f 5 5 5 5 5 5 f 5 f . . 
+. f 5 f 1 f f f f f f 1 f 5 f . 
+f 4 4 f 1 1 1 1 1 1 1 1 f 4 4 f 
+. f 4 4 f f f 1 1 f f f 4 4 f . 
+. f 4 4 4 4 4 f f 4 4 4 4 4 f . 
+. f 4 4 4 4 4 4 4 4 4 4 4 4 f . 
+. . f f f f f f f f f f f f . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "fair":
+            case "anim1":return [img`
+. . . . f f . . . . f f . . . . 
+. . . . f 2 f f f f 2 f . . . . 
+. . . f 2 2 2 2 2 2 2 2 f . . . 
+. . f 2 2 2 2 2 2 2 2 2 2 f . . 
+. . f 5 f 5 5 5 5 5 5 f 5 f . . 
+. f 5 f 1 f 5 5 5 5 f 1 f 5 f . 
+f 5 5 f 1 1 f 5 5 f 1 1 f 5 5 f 
+f 5 5 5 f f f 5 5 f f f 5 5 5 f 
+. f 5 5 5 5 5 5 5 5 5 5 5 5 f . 
+. . f 5 f 5 5 5 5 5 5 f 5 f . . 
+. f 5 f 1 f f f f f f 1 f 5 f . 
+f 4 4 f 1 1 1 1 1 1 1 1 f 4 4 f 
+. f 4 4 f f f 1 1 f f f 4 4 f . 
+. f 4 4 4 4 4 f f 4 4 4 4 4 f . 
+. f 4 4 4 4 4 4 4 4 4 4 4 4 f . 
+. . f f f f f f f f f f f f . . 
+`, img`
+. . . . f f . . . . f f . . . . 
+. . . . f 2 f f f f 2 f . . . . 
+. . . f 2 2 2 2 2 2 2 2 f . . . 
+. . f 2 2 2 2 2 2 2 2 2 2 f . . 
+. . f 5 f 5 5 5 5 5 5 f 5 f . . 
+. f 5 f 1 f 5 5 5 5 f 1 f 5 f . 
+f 5 5 f 1 1 f 5 5 f 1 1 f 5 5 f 
+f 5 5 5 f f f 5 5 f f f 5 5 5 f 
+. f 5 5 5 5 5 5 5 5 5 5 5 5 f . 
+. . f 5 5 f f f f f f 5 5 f . . 
+. f 5 5 f 1 2 2 2 2 1 f 5 5 f . 
+f 4 4 4 f 2 2 2 2 2 2 f 4 4 4 f 
+. f 4 4 4 f f f f f f 4 4 4 f . 
+. f 4 4 4 4 4 4 4 4 4 4 4 4 f . 
+. f 4 4 4 4 4 4 4 4 4 4 4 4 f . 
+. . f f f f f f f f f f f f . . 
+`, img`
+. . . . f f . . . . f f . . . . 
+. . . . f 2 f f f f 2 f . . . . 
+. . . f 2 2 2 2 2 2 2 2 f . . . 
+. . f 2 2 2 2 2 2 2 2 2 2 f . . 
+. . f 5 f 5 5 5 5 5 5 f 5 f . . 
+. f 5 f 1 f 5 5 5 5 f 1 f 5 f . 
+f 5 5 f 1 1 f 5 5 f 1 1 f 5 5 f 
+f 5 5 5 f f f 5 5 f f f 5 5 5 f 
+. f 5 5 5 5 5 f f 5 5 5 5 5 f . 
+. . f 5 f f f 2 2 f f f 5 f . . 
+. f 5 f 1 2 4 4 4 4 2 1 f 5 f . 
+f 4 4 f 2 2 5 4 4 5 2 2 f 4 4 f 
+. f 4 4 f 5 4 5 5 4 5 f 4 4 f . 
+. f 4 4 4 f 5 4 4 5 f 4 4 4 f . 
+. f 4 4 4 4 4 2 5 4 4 4 4 4 f . 
+. . f f f f f 5 2 f f f f f . . 
+`, img`
+. . . . f f . . . . f f . . . . 
+. . . . f 2 f f f f 2 f . . . . 
+. . . f 2 2 2 2 2 2 2 2 f . . . 
+. . f 2 2 2 2 2 2 2 2 2 2 f . . 
+. . f 5 f 5 5 5 5 5 5 f 5 f . . 
+. f 5 f 1 f 5 5 5 5 f 1 f 5 f . 
+f 5 5 f 1 1 f 5 5 f 1 1 f 5 5 f 
+f 5 5 5 f f f 5 5 f f f 5 5 5 f 
+. f 5 5 5 5 5 5 5 5 5 5 5 5 f . 
+. . f 5 5 f f f f f 5 5 5 f . . 
+. f 5 5 f 1 2 2 2 1 f 5 5 5 f . 
+f 4 4 f 2 2 d d d 2 2 f 4 4 4 f 
+. f 4 4 f d d d d d f 4 4 4 f . 
+. f 4 4 d d d d d d d 4 4 4 f . 
+. f 4 4 4 d d d d d 4 4 4 4 f . 
+. . f f f f d d d f f f f f . . 
+`, img`
+. . . . f f . . . . f f . . . . 
+. . . . f 2 f f f f 2 f . . . . 
+. . . f 2 2 2 2 2 2 2 2 f . . . 
+. . f 2 2 2 2 2 2 2 2 2 2 f . . 
+. . f 5 f 5 5 5 5 5 5 f 5 f . . 
+. f 5 f 1 f 5 5 5 5 f 1 f 5 f . 
+f 5 5 f 1 1 f 5 5 f 1 1 f 5 5 f 
+f 5 5 5 f f f 5 5 f f f 5 5 5 f 
+. f 5 5 5 5 5 5 5 5 5 5 5 5 f . 
+. . f 5 5 f f f f f 5 5 5 f . . 
+. f 5 5 f 1 2 2 2 1 f 5 5 5 f . 
+f 4 4 f 2 2 2 2 2 2 2 f 4 4 4 f 
+. f 4 4 f 2 2 2 2 2 f 4 4 4 f . 
+. f 4 4 d d d d d d d 4 4 4 f . 
+. f 4 4 4 d d d d d 4 4 4 4 f . 
+. . f f f f d d d f f f f f . . 
+`, img`
+. . . . f f . . . . f f . . . . 
+. . . . f 2 f f f f 2 f . . . . 
+. . . f 2 2 2 2 2 2 2 2 f . . . 
+. . f 2 2 2 2 2 2 2 2 2 2 f . . 
+. . f 5 f 5 5 5 5 5 5 f 5 f . . 
+. f 5 f 1 f 5 5 5 5 f 1 f 5 f . 
+f 5 5 f 1 1 f 5 5 f 1 1 f 5 5 f 
+f 5 5 5 f f f 5 5 f f f 5 5 5 f 
+. f 5 5 5 5 5 5 5 5 5 5 5 5 f . 
+. . f 5 5 f f f f f 5 5 5 f . . 
+. f 5 5 f 1 2 2 2 1 f 5 5 5 f . 
+f 4 4 f 2 2 2 2 2 2 2 f 4 4 4 f 
+. f 4 4 f 2 2 2 2 2 f 4 4 4 f . 
+. f 4 4 4 f f f f f 4 4 4 4 f . 
+. f 4 4 4 d d d d d 4 4 4 4 f . 
+. . f f f f d d d f f f f f . . 
+`, img`
+. . . . f f . . . . f f . . . . 
+. . . . f 2 f f f f 2 f . . . . 
+. . . f 2 2 2 2 2 2 2 2 f . . . 
+. . f 2 2 2 2 2 2 2 2 2 2 f . . 
+. . f 5 f 5 5 5 5 5 5 f 5 f . . 
+. f 5 f 1 f 5 5 5 5 f 1 f 5 f . 
+f 5 5 f 1 1 f 5 5 f 1 1 f 5 5 f 
+f 5 5 5 f f f 5 5 f f f 5 5 5 f 
+. f 5 5 5 5 5 5 5 5 5 5 5 5 f . 
+. . f 5 5 f f f f f 5 5 5 f . . 
+. f 5 5 f 1 2 2 2 1 f 5 5 5 f . 
+f 4 4 f 2 2 2 2 2 2 2 f 4 4 4 f 
+. f 4 4 f 2 2 2 2 2 f 4 4 4 f . 
+. f 4 4 4 f f f f f 4 4 4 4 f . 
+. f 4 4 4 4 4 4 4 4 4 4 4 4 f . 
+. . f f f f d d d f f f f f . . 
+`, img`
+. . . . f f . . . . f f . . . . 
+. . . . f 2 f f f f 2 f . . . . 
+. . . f 2 2 2 2 2 2 2 2 f . . . 
+. . f 2 2 2 2 2 2 2 2 2 2 f . . 
+. . f 5 f 5 5 5 5 5 5 f 5 f . . 
+. f 5 f 1 f 5 5 5 5 f 1 f 5 f . 
+f 5 5 f 1 1 f 5 5 f 1 1 f 5 5 f 
+f 5 5 5 f f f 5 5 f f f 5 5 5 f 
+. f 5 5 5 5 5 5 5 5 5 5 5 5 f . 
+. . f 5 5 f f f f f 5 5 5 f . . 
+. f 5 5 f 1 2 2 2 1 f 5 5 5 f . 
+f 4 4 f 2 2 2 2 2 2 2 f 4 4 4 f 
+. f 4 4 f 2 2 2 2 2 f 4 4 4 f . 
+. f 4 4 4 f f f f f 4 4 4 4 f . 
+. f 4 4 4 4 4 4 4 4 4 4 4 4 f . 
+. . f f f f f f f f f f f f . . 
+`, img`
+. f 5 f 1 f 5 5 5 5 f 1 f 5 f . 
+f 5 5 f 1 1 f 5 5 f 1 1 f 5 5 f 
+f 5 5 5 f f f 5 5 f f f 5 5 5 f 
+. f 5 5 5 5 5 5 5 5 5 5 5 5 f . 
+. . f 5 5 f f f f f 5 5 5 f . . 
+. f 5 5 f 1 2 2 2 1 f 5 5 5 f . 
+f 4 4 f 2 2 2 2 2 2 2 f 4 4 4 f 
+. f 4 4 f 2 2 2 2 2 f 4 4 4 f . 
+. f 4 4 4 f f f f f 4 4 4 4 f . 
+. f 4 4 4 4 4 4 4 4 4 4 4 4 f . 
+. . f f f f f f f f f f f f . . 
+. . 2 5 5 4 4 4 4 4 5 5 5 2 . . 
+. 2 2 5 5 5 4 4 4 5 5 5 2 2 . . 
+. 2 2 2 5 2 2 4 2 2 5 5 2 2 . . 
+. . . 2 5 2 2 2 2 . 2 2 2 . . . 
+. . . 2 2 . . 2 . . . 2 . . . . 
+`, img`
+. . f 5 5 f f f f f 5 5 5 f . . 
+. f 5 5 f 1 2 2 2 1 f 5 5 5 f . 
+f 4 4 f 2 2 2 2 2 2 2 f 4 4 4 f 
+. f 4 4 f 2 2 2 2 2 f 4 4 4 f . 
+. f 4 4 4 f f f f f 4 4 4 4 f . 
+. f 4 4 4 4 4 4 4 4 4 4 4 4 f . 
+. . f f f f f f f f f f f f . . 
+. . 2 5 5 4 4 4 4 4 4 5 5 2 . . 
+. . 2 5 4 4 4 4 4 4 4 5 5 2 2 . 
+. 2 2 4 4 4 4 4 4 4 4 4 5 2 2 . 
+. 2 2 5 4 4 4 4 4 4 4 5 5 5 2 . 
+. . 2 5 5 4 4 4 4 4 5 5 5 5 . . 
+. 2 2 5 5 5 4 4 4 5 5 5 2 2 . . 
+. 2 2 2 5 2 2 4 2 2 5 5 2 2 . . 
+. . . 2 5 2 2 2 2 . 2 2 2 . . . 
+. . . 2 2 . . 2 . . . 2 . . . . 
+`, img`
+. f 4 4 4 f f f f f 4 4 4 4 f . 
+. f 4 4 4 4 4 4 4 4 4 4 4 4 f . 
+. . f f f f f f f f f f f f . . 
+. . 2 5 5 4 4 4 4 4 4 5 5 2 . . 
+. . 2 5 4 4 4 4 4 4 4 5 5 2 2 . 
+. 2 2 4 4 4 4 4 4 4 4 4 5 2 2 . 
+. 2 2 5 4 4 4 4 4 4 4 5 5 5 2 . 
+. . 2 5 5 4 4 4 4 4 5 5 5 5 . . 
+. 2 2 5 5 5 4 4 4 5 5 5 2 2 . . 
+. 2 2 2 5 2 2 4 2 2 5 5 2 2 . . 
+. . . 2 5 2 2 2 2 . 2 2 2 . . . 
+. . . 2 2 . . 2 . . . 2 . . . . 
+. . . . . . . . . . . . . . . . 
+. . . d d d . . . . . . d d . . 
+d d d d d d d d . . . . d d . . 
+d d d d d d d d d d d d d d d d 
+`, img`
+. . . 2 2 . . 2 . . . 2 . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . d . . . . . . 
+. . . . . . . . . d d . . . . . 
+. . d . . d . . . . d . d . . d 
+. . d . d d d d d d d . d . d d 
+. d d d d d d d d d . d d d d d 
+. d d d d d d d d d d d d . d . 
+. d d d d d d d d d d d d d d . 
+. . . d d d d d d d d d d d . . 
+. . . d d d d d d d d d d d . . 
+d d d d d d d d d d d d d d . . 
+d d d d d d d d d d d d d d d d 
+`];
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Código generado automáticamente. No editar.
